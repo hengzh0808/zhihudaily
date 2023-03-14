eb6ad18ca9d11ed62ed4fb41f3cc4c6007704f36
zhangheng09 <zhangheng09@kuaishou.com>
2023-03-14 19:36:41 +0800

    状态栏取消半透明

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:zhihudaily/home/DailyHome.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    )
+  );
   runApp(const MyApp());
 }
 
